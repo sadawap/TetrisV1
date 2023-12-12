ee68d8a26dc8d1d8ba61379bce7ec660c1767ffe
sadawap <136122624+sadawap@users.noreply.github.com>
2023-12-13 00:39:47 +0530

    Update nodegroup.tf

--- Eks-terraform/nodegroup.tf
+++ Eks-terraform/nodegroup.tf
@@ -38,9 +38,9 @@ resource "aws_eks_node_group" "nodeserver" {
   subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 2
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
   }
   instance_types = [ "t2.micro"]
 
